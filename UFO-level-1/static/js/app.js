0b598a6762482d8da11abd4d1785f2f87d8cc5c2
Maria Barrera <mbarrera_usa@yahoo.com>
2021-03-19 13:14:24 -0700

    javascript-challenge modification
    20210319 – create index.html file

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,37 @@
+// Assign the data from `data.js` to a descriptive variable
+var tableData = data;
+
+// js code here..... reference: student activity JS - day 3 - activity 9
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#patient-form-input");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value")
+};
+
+  // Use the form input to filter the data by date
+  function bloodType(inputValue){
+    return inputValue.bloodType
+  }
+
+  var filteredData = data.filter(data =>data.bloodType ==inputValue);
+
+
+};
